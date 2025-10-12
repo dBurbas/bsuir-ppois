d8b72f5baf14d65c0416268c27465d4c44ed83a2
Frostnout <dima.burbas@gmail.com>
2025-10-12 22:14:46 +0300

    Feat: add BinarySearchTree template class declaration

---
+++ lab1/eng_rus_dictionary/src/binary_search_tree.hpp
@@ -0,0 +1,67 @@
+//
+//  binary_search_tree.hpp
+//  eng_rus_dictionary
+//
+//  Created by Dmitry Burbas on 10/10/2025.
+//
+
+#ifndef BINARY_SEARCH_TREE_HPP
+#define BINARY_SEARCH_TREE_HPP
+
+#include <cstddef>
+
+template <typename KeyType,typename ValueType>
+
+class BinarySearchTree{
+public:
+    BinarySearchTree(){};
+    
+    BinarySearchTree(const BinarySearchTree& other);
+    BinarySearchTree& operator=(const BinarySearchTree& other);
+    
+    ~BinarySearchTree();
+    
+    bool operator==(const BinarySearchTree& other);
+    
+    bool operator!=(const BinarySearchTree& other);
+    
+    bool Insert(const KeyType& key, const ValueType& value);
+    
+    friend std::ostream& operator<<(std::ostream out_stream, const BinarySearchTree& tree);
+    
+    friend std::istream& operator<<(std::ostream out_stream, BinarySearchTree& tree);
+    
+    const ValueType* Find(const KeyType& key) const;
+    
+    bool Erase(const KeyType& key);
+    
+    void Clear();
+    
+    void PrintInOrder() const;
+    
+    bool IsEmpty() const;
+    
+    size_t GetSize() const;
+    
+private:
+    struct Node{
+        KeyType key;
+        ValueType value;
+        Node* left_child = nullptr;
+        Node* right_child = nullptr;
+    };
+    Node* root_;
+    size_t size_;
+    
+    Node* ClearHelper(Node* node);
+    
+    Node* CopyHelper(const Node* node);
+    
+    void PrintInOrderHelper(const Node* node) const;
+    
+    Node* EraseHelper(Node* node, const KeyValue& key, bool& erased);
+    
+    bool EqualsHelper(const Node* node1, const Node* node2) const;
+};
+
+#endif // !BINARY_SEARCH_TREE_HPP
