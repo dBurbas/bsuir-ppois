cd870c13b2454b59699162c32281ba3ed7d788fa
Frostnout <dima.burbas@gmail.com>
2025-10-12 22:25:42 +0300

    Fix: correct syntax errors in BinarySearchTree class declaration
    - Add #include <iostream> for >> and << operators
    - Fix friend function signatures (add & to parameters)
    - Fix operator>> declaration typo (was operator<<)
    - Fix typo KeyValue -> KeyType (EraseHelper)
    - Change ClearHelper return type to void
    - Add const to comparison methods
    - Fix endif comment \\ !BINARY_SEARCH_TREE_HPP -> \\ BINARY_SEARCH_TREE_HPP

--- lab1/eng_rus_dictionary/src/binary_search_tree.hpp
+++ lab1/eng_rus_dictionary/src/binary_search_tree.hpp
@@ -9,6 +9,7 @@
 #define BINARY_SEARCH_TREE_HPP
 
 #include <cstddef>
+#include <iostream>
 
 template <typename KeyType,typename ValueType>
 
@@ -21,15 +22,15 @@ public:
     
     ~BinarySearchTree();
     
-    bool operator==(const BinarySearchTree& other);
+    bool operator==(const BinarySearchTree& other) const;
     
-    bool operator!=(const BinarySearchTree& other);
+    bool operator!=(const BinarySearchTree& other) const;
     
     bool Insert(const KeyType& key, const ValueType& value);
     
-    friend std::ostream& operator<<(std::ostream out_stream, const BinarySearchTree& tree);
+    friend std::ostream& operator<<(std::ostream& out_stream, const BinarySearchTree& tree);
     
-    friend std::istream& operator<<(std::ostream out_stream, BinarySearchTree& tree);
+    friend std::istream& operator>>(std::ostream& in_stream, BinarySearchTree& tree);
     
     const ValueType* Find(const KeyType& key) const;
     
@@ -53,15 +54,15 @@ private:
     Node* root_;
     size_t size_;
     
-    Node* ClearHelper(Node* node);
+    void ClearHelper(Node* node);
     
     Node* CopyHelper(const Node* node);
     
     void PrintInOrderHelper(const Node* node) const;
     
-    Node* EraseHelper(Node* node, const KeyValue& key, bool& erased);
+    Node* EraseHelper(Node* node, const KeyType& key, bool& erased);
     
     bool EqualsHelper(const Node* node1, const Node* node2) const;
 };
 
-#endif // !BINARY_SEARCH_TREE_HPP
+#endif // BINARY_SEARCH_TREE_HPP
