ba92186a62f824cd9f52fa1fdf30d9158b3c4154
Frostnout <dima.burbas@gmail.com>
2025-10-18 17:05:41 +0300

    feat(dictionary): add Dictionary class header
    Description:
    Add dictionary.h file, declare public and private methods and field for
    dictionary container.

---
+++ lab1/eng_rus_dictionary/src/dictionary/dictionary.h
@@ -0,0 +1,49 @@
+//
+//  dictionary.h
+//  eng_rus_dictionary
+//
+//  Created by Dmitry Burbas on 17/10/2025.
+//
+
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+
+#include <cstddef>
+#include <iostream>
+#include <string>
+#include "../binary_search_tree/binary_search_tree.hpp"
+
+class Dictionary {
+public:
+    Dictionary() = default;
+    
+    Dictionary(const Dictionary& other);
+    Dictionary& operator=(const Dictionary& other);
+    
+    ~Dictionary() = default;
+    
+    Dictionary& operator+=(const std::pair<std::string, std::string>& word_pair);
+    Dictionary& operator+=(const std::string& word_pair);
+    Dictionary& operator+=(const char* word_pair);
+    
+    Dictionary& operator-=(const std::string& english_word);
+    Dictionary& operator-=(const char* english_word);
+    
+    const std::string& operator[](const std::string& english_word) const;
+    std::string& operator[](const std::string& english_word);
+    
+    bool operator==(const Dictionary& other) const;
+    bool operator!=(const Dictionary& other) const;
+    
+    friend std::ostream& operator<<(std::ostream& out_stream, const Dictionary& dict);
+    friend std::istream& operator>>(std::istream& in_stream, Dictionary& dict);
+    
+    void Clear();
+    size_t GetSize() const;
+    bool IsEmpty() const;
+    bool LoadFromFile(const std::string& filename);
+private:
+    BinarySearchTree<std::string, std::string> container_;
+};
+
+#endif // DICTIONARY_H
