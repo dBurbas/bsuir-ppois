12f41b429ebab9d2d516ed8ea438a1cd56cefe7b
Frostnout <dima.burbas@gmail.com>
2025-10-22 13:19:31 +0300

    feat(dictionary): add main.cc

---
+++ lab1/eng_rus_dictionary/src/main.cc
@@ -0,0 +1,11 @@
+/**
+ * @file main.cc
+ * @brief main function to run program
+ * @author Dmitry Burbas
+ * @date 09/10/2025
+ */
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
