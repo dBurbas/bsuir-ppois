3881042046684b79250d56f0fd92a90f82f3d208
Frostnout <dima.burbas@gmail.com>
2025-10-22 18:08:59 +0300

    test(dictionary): add dictionary unit tests
    Description:
    - Add google unit tests for Dictionary class
    - Add google unit tests for dictionary utils functions
    - Add main_test.cc file

---
+++ lab1/eng_rus_dictionary/tests/main_test.cc
@@ -0,0 +1,13 @@
+//
+//  main.cc
+//  tests
+//
+//  Created by Dmitry Burbas on 17/10/2025.
+//
+#include <gtest/gtest.h>
+#include <iostream>
+
+int main(int argc, char * argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
