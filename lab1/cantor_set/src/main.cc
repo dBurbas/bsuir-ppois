7bc249f36d4b565c3f7d5a95b33cbe8cf4123665
Frostnout <dima.burbas@gmail.com>
2025-10-26 21:55:08 +0300

    feat(lab1/set): add main.cc file

---
+++ lab1/cantor_set/src/main.cc
@@ -0,0 +1,14 @@
+/**
+ * @file main.cc
+ * @brief Main file of program
+ * @author Dmitry Burbas
+ * @date 09/10/2025
+ */
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
