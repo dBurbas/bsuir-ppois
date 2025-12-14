ef5d4b57dd045fd03a815a1e52e26d3eb8386ec8
Frostnout <dima.burbas@gmail.com>
2025-12-14 18:41:57 +0300

    feat[lab2]: add new exceptions for new classes

--- lab2/src/exceptions/dean_office_exceptions.h
+++ lab2/src/exceptions/dean_office_exceptions.h
@@ -6,6 +6,8 @@
  */
 #ifndef DEAN_OFFICE_EXCEPTIONS_H
 #define DEAN_OFFICE_EXCEPTIONS_H
+// TODO: поменять названия исключений и добавить конкретные например "Нет даты
+// рождения человека"
 
 #include "base_exception.h"
 /**
@@ -22,12 +24,12 @@ class DeanOfficeException : public BaseException {
 };
 
 /**
- * @class PersonException
+ * @class PersonComponentException
  * @brief Класс исключений для некорректных операций с Person
  * @details Выбрасывается когда добавляется некорректная персональная или
  * контактная информация для человека.
  */
-class PersonException : public DeanOfficeException {
+class PersonComponentException : public DeanOfficeException {
  public:
   using DeanOfficeException::DeanOfficeException;  ///< Наследование
                                                    ///< конструкторов
@@ -44,7 +46,16 @@ class EmployeeException : public DeanOfficeException {
   using DeanOfficeException::DeanOfficeException;  ///< Наследование
                                                    ///< конструкторов
 };
-
+/**
+ * @class StudentException
+ * @brief Класс исключений для некорректных операций с Student
+ * @details Выбрасывается при
+ */
+class StudentException : public DeanOfficeException {
+ public:
+  using DeanOfficeException::DeanOfficeException;  ///< Наследование
+                                                   ///< конструкторов
+};
 /**
  * @class DocumentException
  * @brief Класс исключений для некорректных операций с Document
@@ -57,4 +68,73 @@ class DocumentException : public DeanOfficeException {
                                                    ///< конструкторов
 };
 
+/**
+ * @class OrderArgumentsException
+ * @brief Класс исключений при отсутствии основания для приказа
+ * @details Выбрасывается при создании приказа без основания
+ */
+class OrderArgumentsException : public DeanOfficeException {
+ public:
+  using DeanOfficeException::DeanOfficeException;  ///< Наследование
+                                                   ///< конструкторов
+};
+
+/**
+ * @class ReportPeriodException
+ * @brief Класс исключений для некорректного ввода периода отчета
+ * @details Выбрасывается при некорректном вводе периода отчета
+ */
+class ReportPeriodException : public DeanOfficeException {
+ public:
+  using DeanOfficeException::DeanOfficeException;  ///< Наследование
+                                                   ///< конструкторов
+};
+
+/**
+ * @class SalaryException
+ * @brief Класс исключений для некорректных операций с зарплатой
+ * @details Выбрасывается при создании зарплаты с негативным значением оклада
+ * или не положительным значением ставки.
+ */
+class SalaryException : public DeanOfficeException {
+ public:
+  using DeanOfficeException::DeanOfficeException;  ///< Наследование
+                                                   ///< конструкторов
+};
+
+/**
+ * @class ScholarshipException
+ * @brief Класс исключений для некорректных операций со стипендией
+ * @details Выбрасывается при создании стипендии с негативным значением базового
+ * значения или не положительным значением коэффициентом надбавки.
+ */
+class ScholarshipException : public DeanOfficeException {
+ public:
+  using DeanOfficeException::DeanOfficeException;  ///< Наследование
+                                                   ///< конструкторов
+};
+
+/**
+ * @class SubjectException
+ * @brief Класс исключений для некорректных операций с предметами
+ * @details Выбрасывается при пустых id и имени предмета и других операциях с
+ * Subject
+ */
+class SubjectException : public DeanOfficeException {
+ public:
+  using DeanOfficeException::DeanOfficeException;  ///< Наследование
+                                                   ///< конструкторов
+};
+
+/**
+ * @class EventException
+ * @brief Класс исключений для некорректных операций с событиями
+ * @details Выбрасывается при некорректных операциях с Event, например дата
+ * начала позже даты конца и т.п.
+ */
+class EventException : public DeanOfficeException {
+ public:
+  using DeanOfficeException::DeanOfficeException;  ///< Наследование
+  ///< конструкторов
+};
 #endif  // DEAN_OFFICE_EXCEPTIONS_H
