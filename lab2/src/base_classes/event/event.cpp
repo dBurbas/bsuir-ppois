523f6e71cb39dbd91b44eeae4b190013b995d35f
Frostnout <dima.burbas@gmail.com>
2025-12-14 18:50:33 +0300

    feat[lab2]: first implement of event

--- lab2/src/base_classes/event/event.cpp
+++ lab2/src/base_classes/event/event.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "event.h"
+void Event::CleanupExpired() {
+  participants_.erase(std::remove_if(participants_.begin(), participants_.end(),
+                                     [](const std::weak_ptr<Person>& wp) {
+                                       return wp.expired();
+                                     }),
+                      participants_.end());
+}
+
+void Event::AddParticipant(const std::shared_ptr<Person>& p) {
+  if (!p) throw EventException("Null participant");
+  CleanupExpired();
+  for (const auto& wp : participants_) {
+    if (!wp.owner_before(p) && !p.owner_before(wp)) {
+      throw EventException("Already existed participant");
+    }
+  }
+  participants_.push_back(p);
+}
--- lab2/src/base_classes/event/event.h
+++ lab2/src/base_classes/event/event.h
@@ -1,21 +1,47 @@
-//
-// Created by Dmitry Burbas on 16/11/2025.
-//
-
+/**
+ * @file event.h
+ * @brief Базовый класс для событий
+ * @author Dmitry Burbas
+ * @date 16/11/2025
+ */
 #ifndef EVENT_H
 #define EVENT_H
+#include <algorithm>
+#include <memory>
 #include <vector>
 
+#include "../../exceptions/dean_office_exceptions.h"
 #include "../person/person.h"
-// TODO:написать логику методов
+// TODO: документация
 class Event {
  public:
+  Event(std::string title, const Date& date, const Date& end,
+        const int hour_duration, Address address)
+      : title_(std::move(title)),
+        event_date_(date),
+        hour_duration_(hour_duration),
+        address_(std::move(address)) {
+    if (title_.empty()) throw EventException("Event title is empty");
+    if (hour_duration_ <= 0) throw EventException("Event duration must be > 0");
+  }
+
+  virtual ~Event() = default;
+
+  void AddParticipant(const std::shared_ptr<Person>& p);
+  void CleanupExpired();
+
+  void RemoveAllParticipants() { participants_.clear(); }
+
+  [[nodiscard]] const std::string& GetTitle() const { return title_; }
+  [[nodiscard]] const Date& GetEventDate() const { return event_date_; }
+  [[nodiscard]] int GetHourDuration() const { return hour_duration_; }
+  [[nodiscard]] const Address& GetAddress() const { return address_; }
+
  protected:
-  std::vector<Person*> participants_;
+  std::vector<std::weak_ptr<Person>> participants_;
   std::string title_;
-  Date start_date_;
-  Date end_date_;
-  int hour_duration;
+  Date event_date_;
+  int hour_duration_;
   Address address_;
 };
 
