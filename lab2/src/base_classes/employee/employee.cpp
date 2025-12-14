4878ad21695333ca8f56ccf55a88e551c4beca01
Frostnout <dima.burbas@gmail.com>
2025-12-14 18:46:34 +0300

    feat[lab2]: implement employee class

--- lab2/src/base_classes/employee/employee.cpp
+++ lab2/src/base_classes/employee/employee.cpp
@@ -1,5 +1,44 @@
 //
-// Created by Dmitry Burbas on 06/12/2025.
+// Created by Dmitry Burbas on 14/12/2025.
 //
-
 #include "employee.h"
+
+#include "../../exceptions/dean_office_exceptions.h"
+
+void Employee::BecomeActive() {
+  if (employee_status_ == EmployeeStatus::Fired)
+    throw EmployeeException("Employee error: employee is fired");
+  if (employee_status_ != EmployeeStatus::OnLeave)
+    throw EmployeeException("Employee is not on leave");
+  employee_status_ = EmployeeStatus::Active;
+}
+void Employee::SendOnLeave(const EmployeeOnLeaveOrder& order,
+                           const Date& cur_date) {
+  if (employee_status_ == EmployeeStatus::Fired)
+    throw EmployeeException("Employee error: employee is fired");
+  if (employee_status_ == EmployeeStatus::OnLeave)
+    throw EmployeeException("Employee error: already on leave");
+  if (!order.IsSigned())
+    throw EmployeeException("Employee error: order is not signed");
+  if (order.GetEffectiveDate() > cur_date)
+    throw EmployeeException("Employee error: order has not taken effect");
+  employee_status_ = EmployeeStatus::OnLeave;
+}
+void Employee::Fire(const DismissalOrder& order, const Date& cur_date) {
+  if (employee_status_ == EmployeeStatus::Fired)
+    throw EmployeeException("Employee error: already fired");
+  if (!order.IsSigned())
+    throw EmployeeException("Employee error: order is not signed");
+  if (order.GetEffectiveDate() > cur_date)
+    throw EmployeeException("Employee error: order has not taken effect");
+  employee_status_ = EmployeeStatus::Fired;
+}
+int Employee::FullYearsBetween(const Date& from, const Date& to) {
+  if (to < from) throw EmployeeException("Invalid dates for experience");
+  int years = to.GetYear() - from.GetYear();
+  if (to.GetMonth() < from.GetMonth() ||
+      (to.GetMonth() == from.GetMonth() && to.GetDay() < from.GetDay())) {
+    --years;
+  }
+  return years;
+}
--- lab2/src/base_classes/employee/employee.h
+++ lab2/src/base_classes/employee/employee.h
@@ -1,44 +1,55 @@
-//
-// Created by Dmitry Burbas on 06/12/2025.
-//
-
-#ifndef LAB2_EMPLOYEE_H
-#define LAB2_EMPLOYEE_H
+/**
+ * @file employee.h
+ * @brief Базовый класс для работников
+ * @author Dmitry Burbas
+ * @date 06/12/2025
+ */
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+#include "../../documents/orders/dismissal_order/dismissal_order.h"
+#include "../../documents/orders/employee_on_leave_order/employee_on_leave_order.h"
+#include "../../enum_classes/employee_status.h"
+#include "../../salary/salary.h"
+#include "../order/order.h"
 #include "../person/person.h"
-// TODO: перепроверить и уточнить методы и поля
+
+// TODO: сделать документацию
 class Employee : public Person {
  public:
-  explicit Employee(const Person& employee) : Person(employee) {}
-  Employee(const Person& employee, double salary, double salary_rate)
-      : Person(employee), salary_(salary), salary_rate_(salary_rate) {}
+  Employee(const PersonalInfo& personal_info, const ContactInfo& contact_info,
+           const double salary_base, const double salary_rate,
+           const Date& hire_date)
+      : Person(personal_info, contact_info),
+        salary_(salary_base, salary_rate),
+        hire_date_(hire_date) {}
+
   ~Employee() override = default;
   virtual void Work() = 0;
-  void IncreaseExperienceValue(int experience_increase) {
-    if (experience_increase < 0) {
-      throw EmployeeException("Employee experience increase must be > 0");
-    }
-    experience_years_ += experience_increase;
+  void BecomeActive();
+  void SendOnLeave(const EmployeeOnLeaveOrder& order, const Date& cur_date);
+  void Fire(const DismissalOrder& order, const Date& cur_date);
+  void SetSalaryBase(const double new_salary_base) {
+    salary_.SetBase(new_salary_base);
   }
-  void SetSalaryValue(double new_salary) {
-    if (new_salary < 0.0) {
-      throw EmployeeException("Employee error: salary cannot be negative");
-    }
-    salary_ = new_salary;
+  void SetSalaryRate(const double new_salary_rate) {
+    salary_.SetRate(new_salary_rate);
   }
-  void SetSalaryRate(double new_salary_rate) {
-    if (new_salary_rate <= 0.0) {
-      throw EmployeeException("Employee error: salary must be positive");
-    }
-    salary_rate_ = new_salary_rate;
-  }
-  [[nodiscard]] int GetExperienceValue() const { return experience_years_; }
-  [[nodiscard]] double GetSalaryValue() const { return salary_; }
-  [[nodiscard]] double GetSalaryRate() const { return salary_rate_; }
 
- protected:
-  int experience_years_ = 0;
-  double salary_ = 0.0;
-  double salary_rate_ = 1.0;
+  [[nodiscard]] EmployeeStatus GetEmployeeStatus() const {
+    return employee_status_;
+  }
+  [[nodiscard]] int GetExperienceYears(const Date& cur_date) const {
+    return FullYearsBetween(hire_date_, cur_date);
+  }
+  [[nodiscard]] double GetSalary() const { return salary_.GetSalary(); }
+  [[nodiscard]] double GetSalaryBase() const { return salary_.GetBase(); }
+  [[nodiscard]] double GetSalaryRate() const { return salary_.GetRate(); }
+
+ private:
+  static int FullYearsBetween(const Date& from, const Date& to);
+  Salary salary_;
+  Date hire_date_;
+  EmployeeStatus employee_status_ = EmployeeStatus::Active;
 };
 
-#endif  // LAB2_EMPLOYEE_H
+#endif  // EMPLOYEE_H
